d5c606e90c1fa813e57069c35b7e8b4f6640ee1d
bluesquarez <eewinterz@gmail.com>
2017-11-26 18:08:51 -0500

    Upload Files
    Uploading some of the course files I completed. These were fairly simple and were pretty easy to finish up.

---
+++ PrintingChoices2.java
@@ -0,0 +1,10 @@
+public class PrintingChoices2 {
+	public static void main(String[] arguments) {
+		/*This second version of the PrintingChoices is a study drills assignment. */
+		System.out.println("Alpha\nBravo\nCharlie\nDelta\n\nEchoFoxtrot\nGolfHotel\nIndia\n");
+		
+		System.out.println();
+		System.out.println("This" + " " + "is" + " " + "a" + " test.");
+		System.out.println("I am learning" + " " + " Java the Hard" + " Way!" );
+	}
+}
