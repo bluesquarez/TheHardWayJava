e74c05eec6c533b243de2b5ebed14b7cbdbdd83d
bluesquarez <eewinterz@gmail.com>
2017-11-26 18:00:41 -0500

    FirstProgram
    This is the first program we create in the course.

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main(String[] arguments) {
+		System.out.println("I, John Doe, am determined to learn how to code.");
+		System.out.println("Today's date is: 11/26/2017");
+	}
+}
